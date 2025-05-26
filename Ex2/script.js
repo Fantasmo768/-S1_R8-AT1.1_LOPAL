1457c960076d029f7cf86f00a8c0436ced466213
unknown <sopesquisa133@gmail.com>
2025-05-26 13:28:19 -0300

    Versão sem erros
    Esse commit é para arrumar alguns pequenos erros que tinham no código

--- Ex2/script.js
+++ Ex2/script.js
@@ -1,23 +1,7 @@
-let numeros = [] //Variável do vetor dos números
-let numero = 0; //Variável que vai pedir o número para se comparar
-let numeroComparar
-let quantidadeNumero = 0;
-let mensagem = [] //Variável da mensagem
+let numeros = [] //Vetor dos números
 
-for (let i = 0; i < 10; i++) { //Looping para pedir os números
-    numero = parseFloat(prompt(`Digite o valor do ${i + 1}° número`)) //Variável que vai pedir os números
-    while (isNaN(numero)) {
-        numero = parseFloat(prompt(`Digite o valor do ${i + 1}° número`))
-    }
-    numeros [i] = numero
-}
-    numeroComparar = parseInt(prompt("Digite um número para comparar")) //Valor que vai pedir o número para se comparar
-
-for (let i = 0; i < 10; i++) { //Looping para checar o valor dos números e comparar
-    if (numeroComparar == numeros[i]) { //Se o número para se comparar for igual o número i, a quantidade de número sobe e acrescenta o valor na mensagem
-        mensagem [quantidadeNumero] = numeros[i] 
-        quantidadeNumero ++
-    }
+for (let i = 0; i < 7; i++) { //Looping para gerar os números
+    numeros [i] = Math.floor(Math.random () * 10) //números que serão gerados
 }
 
-alert(`Esse número apareceu ${quantidadeNumero} vezes e foram ${mensagem}`) //Alerta dos números
+alert (`Os números gerados foram ${numeros}`) //Alerta dos números
--- Ex3/script.js
+++ Ex3/script.js
@@ -1,7 +1,26 @@
-let numeros = [] //Vetor dos números
+let numeros = [] //Variável do vetor dos números
+let numero = 0; //Variável que vai pedir o número para se comparar
+let numeroComparar
+let quantidadeNumero = 0;
+let mensagem = [] //Variável da mensagem
 
-for (let i = 0; i < 7; i++) { //Looping para gerar os números
-    numeros [i] = Math.floor(Math.random () * 10) //números que serão gerados
+for (let i = 0; i < 10; i++) { //Looping para pedir os números
+    numero = parseFloat(prompt(`Digite o valor do ${i + 1}° número`)) //Variável que vai pedir os números
+    while (isNaN(numero)) { //Valor que vai pedir o número de novo caso não seja um número
+        numero = parseFloat(prompt(`Digite o valor do ${i + 1}° número`))
+    }
+    numeros [i] = numero
+}
+    numeroComparar = parseInt(prompt("Digite um número para comparar")) //Valor que vai pedir o número para se comparar
+    while (isNaN(numeroComparar)) { //Valor que vai pedir o número de novo caso não seja um número
+        numeroComparar = parseInt(prompt("Digite um número para comparar"))
+    }
+
+for (let i = 0; i < 10; i++) { //Looping para checar o valor dos números e comparar
+    if (numeroComparar == numeros[i]) { //Se o número para se comparar for igual o número i, a quantidade de número sobe e acrescenta o valor na mensagem
+        mensagem [quantidadeNumero] = numeros[i] 
+        quantidadeNumero ++
+    }
 }
 
-alert (`Os números gerados foram ${numeros}`) //Alerta dos números
+alert(`Esse número apareceu ${quantidadeNumero} vezes e foram ${mensagem}`) //Alerta dos números
